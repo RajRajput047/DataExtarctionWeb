64623cd9919eed92163b285eac2182e6019948f1
Raj Rajput <rajrajput124h@gmail.com>
2026-02-20 01:51:13 +0530

    Fix: Added pip upgrade and set stable python version

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status.
+# Exit on error
 set -o errexit
 
-# Install your Python packages
+# Upgrade pip and setuptools FIRST (Important!)
+python -m pip install --upgrade pip setuptools wheel
+
+# Install other requirements
 pip install -r requirements.txt
 
-# Gather all CSS/JS files into the staticfiles folder
-python manage.py collectstatic --noinput
-
-# Sync your Neon database with your Django models
-python manage.py migratepip install -r requirements.txt
+# Database migrations and static files
 python manage.py collectstatic --noinput
 python manage.py migrate
